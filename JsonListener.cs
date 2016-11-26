9bfc8589b9538b71ff58ae7809e6953d600dea45
mmcer <me@5danyuan.com>
2016-11-26 12:05:02 +0800

    First initialized commit.

---
+++ JsonListener.cs
@@ -0,0 +1,23 @@
+﻿namespace JsonStreamPaser
+{
+    public abstract class JsonListener
+    {
+        public abstract void Whitespace(char c);
+
+        public abstract void StartDocument();
+
+        public abstract void Key(string key);
+
+        public abstract void Value(string value);
+
+        public abstract void EndArray();
+
+        public abstract void EndObject();
+
+        public abstract void EndDocument();
+
+        public abstract void StartArray();
+
+        public abstract void StartObject();
+    }
+}
